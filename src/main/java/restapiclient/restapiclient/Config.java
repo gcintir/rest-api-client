38be7c53128bdcafadb830cd52cd0e84a335e03f
Güray Çintir <guraycintir@gmail.com>
2020-06-04 10:11:14 +0300

    resttemplate timeout config

--- src/main/java/restapiclient/restapiclient/Config.java
+++ src/main/java/restapiclient/restapiclient/Config.java
@@ -1,14 +1,23 @@
 package restapiclient.restapiclient;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class Config {
 
+    static final int TIMEOUT = 15000;
+
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
     }
 }
